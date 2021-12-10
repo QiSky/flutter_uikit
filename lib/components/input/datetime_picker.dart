34b095bc3ea4dcebb49c46f9a13523d8d5355195
qifanxin <2511242638@qq.com>
2021-12-10 17:52:36 +0800

    新增DateTimePicker组件

---
+++ lib/components/input/datetime_picker.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'datetime_picker_item.dart';
+
+class DateTimePicker extends StatefulWidget {
+  final String title;
+
+  final String confirmText;
+
+  final String backText;
+
+  final NcDateMode mode;
+
+  final DateTime? initTime;
+
+  final Function(DateTime confirmTime) confirmCallBack;
+
+  const DateTimePicker(this.title, this.confirmText, this.backText, this.mode,
+      this.initTime, this.confirmCallBack);
+
+  @override
+  State<StatefulWidget> createState() => _DateTimePickerState();
+
+  static void show(BuildContext context,
+      {String title = '创建时间',
+      String confirmText = '确定',
+      String backText = '返回',
+      NcDateMode mode = NcDateMode.ymdhm,
+      DateTime? initTime,
+      required Function(DateTime confirmTime) confirmCallBack}) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) => DateTimePicker(
+            title, confirmText, backText, mode, initTime, confirmCallBack));
+  }
+}
+
+class _DateTimePickerState extends State<DateTimePicker> {
+  late DateTime _dateTime;
+
+  late int days;
+
+  @override
+  void initState() {
+    _dateTime = widget.initTime ?? DateTime.now();
+    days = DateTime(
+            _dateTime.year, _dateTime.month != 12 ? _dateTime.month + 1 : 1, 0)
+        .day;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(44),
+            child: AppBar(
+              elevation: 0.5,
+              backgroundColor: Colors.white,
+              centerTitle: true,
+              leading: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: GestureDetector(
+                      child: Text(
+                        widget.backText,
+                        style: const TextStyle(
+                            fontSize: 16, color: Color.fromRGBO(0, 77, 255, 1)),
+                      ),
+                      onTap: () => Navigator.of(context).pop()),
+                ),
+              ),
+              title: Text(widget.title,
+                  style: const TextStyle(
+                      fontSize: 18,
+                      color: Color.fromRGBO(10, 13, 20, 1),
+                      fontWeight: FontWeight.w500)),
+              actions: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                        widget.confirmCallBack(_dateTime);
+                      },
+                      child: Text(
+                        widget.confirmText,
+                        style: const TextStyle(
+                            fontSize: 16, color: Color.fromRGBO(0, 77, 255, 1)),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              getBody(),
+              Align(
+                  alignment: Alignment.topCenter,
+                  child: IgnorePointer(
+                    child: Container(
+                      width: double.infinity,
+                      height: 48,
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                            Color(0xFFFFFFFF),
+                            Color(0x00FFFFFF),
+                          ])),
+                    ),
+                  )),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: IgnorePointer(
+                  child: Container(
+                    width: double.infinity,
+                    height: 48,
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                          Color(0xFFFFFFFF),
+                          Color(0x00FFFFFF),
+                        ])),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+
+  Widget getBody() {
+    late Widget body;
+    switch (widget.mode) {
+      case NcDateMode.hs:
+        body = Row(
+          children: [
+            DateTimePickerItem(
+                List.generate(24, (index) => index, growable: false), (time) {
+              _dateTime = DateTime(_dateTime.year, _dateTime.month,
+                  _dateTime.day, time, _dateTime.minute);
+            }, '时', defaultValue: _dateTime.hour),
+            DateTimePickerItem(
+                List.generate(60, (index) => index, growable: false), (time) {
+              _dateTime = DateTime(_dateTime.year, _dateTime.month,
+                  _dateTime.day, _dateTime.hour, time);
+            }, '分', defaultValue: _dateTime.minute)
+          ],
+        );
+        break;
+      case NcDateMode.ym:
+        body = Row(
+          children: [
+            DateTimePickerItem(
+                List.generate(100, (index) => DateTime.now().year - 50 + index,
+                    growable: false), (time) {
+              _dateTime = DateTime(time, _dateTime.month, _dateTime.day,
+                  _dateTime.hour, _dateTime.minute);
+            }, '年', defaultValue: _dateTime.year),
+            DateTimePickerItem(
+                List.generate(12, (index) => index + 1, growable: false),
+                (time) {
+              _dateTime = DateTime(_dateTime.year, time, _dateTime.day,
+                  _dateTime.hour, _dateTime.minute);
+            }, '月', defaultValue: _dateTime.month)
+          ],
+        );
+        break;
+      case NcDateMode.ymdhm:
+        body = Row(
+          children: [
+            DateTimePickerItem(
+                List.generate(100, (index) => DateTime.now().year - 50 + index,
+                    growable: false), (time) {
+              _dateTime = DateTime(time, _dateTime.month, _dateTime.day,
+                  _dateTime.hour, _dateTime.minute);
+              setState(() {
+                days = DateTime(time, _dateTime.month, 0).day;
+              });
+            }, '年', defaultValue: _dateTime.year),
+            DateTimePickerItem(
+                List.generate(12, (index) => index + 1, growable: false),
+                (time) {
+              setState(() {
+                if (_dateTime.day >
+                    DateTime(_dateTime.year, time != 12 ? time + 1 : 1, 0)
+                        .day) {
+                  _dateTime = DateTime(
+                      _dateTime.year,
+                      time,
+                      DateTime(_dateTime.year, time != 12 ? time + 1 : 1, 0)
+                          .day,
+                      _dateTime.hour,
+                      _dateTime.minute);
+                } else {
+                  _dateTime = DateTime(_dateTime.year, time, _dateTime.day,
+                      _dateTime.hour, _dateTime.minute);
+                }
+                days =
+                    DateTime(_dateTime.year, time != 12 ? time + 1 : 1, 0).day;
+              });
+            }, '月', defaultValue: _dateTime.month),
+            DateTimePickerItem(
+                List.generate(days, (index) => index + 1, growable: true),
+                (time) {
+              _dateTime = DateTime(_dateTime.year, _dateTime.month, time,
+                  _dateTime.hour, _dateTime.minute);
+            }, '日', defaultValue: _dateTime.day),
+            DateTimePickerItem(
+                List.generate(24, (index) => index, growable: false), (time) {
+              _dateTime = DateTime(_dateTime.year, _dateTime.month,
+                  _dateTime.day, time, _dateTime.minute);
+            }, '时', defaultValue: _dateTime.hour),
+            DateTimePickerItem(
+                List.generate(60, (index) => index, growable: false), (time) {
+              _dateTime = DateTime(_dateTime.year, _dateTime.month,
+                  _dateTime.day, _dateTime.hour, time);
+            }, '分', defaultValue: _dateTime.minute)
+          ],
+        );
+        break;
+    }
+    return body;
+  }
+}
+
+enum NcDateMode {
+  ym,
+  hs,
+  ymdhm,
+}
---
+++ lib/components/input/datetime_picker_item.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DateTimePickerItem extends StatefulWidget {
+  late List<int> timeRangeList;
+
+  late Function(int data) selectedCallBack;
+
+  late String unit;
+
+  int? defaultValue;
+
+  DateTimePickerItem(this.timeRangeList, this.selectedCallBack, this.unit,
+      {this.defaultValue});
+
+  @override
+  State<StatefulWidget> createState() => _DateTimePickerItemState();
+}
+
+class _DateTimePickerItemState extends State<DateTimePickerItem> {
+  int currentIndex = 0;
+
+  FixedExtentScrollController? _controller;
+
+  @override
+  void initState() {
+    if (widget.defaultValue != null) {
+      if (!widget.timeRangeList.contains(widget.defaultValue!)) {
+        currentIndex = widget.timeRangeList.length - 1;
+      } else {
+        currentIndex = widget.timeRangeList.indexOf(widget.defaultValue!);
+      }
+    }
+    if (widget.defaultValue != null) {
+      _controller = FixedExtentScrollController(
+          initialItem: !widget.timeRangeList.contains(widget.defaultValue!)
+              ? widget.timeRangeList.length - 1
+              : widget.timeRangeList.indexOf(widget.defaultValue!));
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentIndex > widget.timeRangeList.length - 1) {
+      currentIndex = widget.timeRangeList.length - 1;
+    }
+    return Expanded(
+        child: CupertinoPicker.builder(
+      useMagnifier: true,
+      diameterRatio: 20,
+      backgroundColor: Colors.white,
+      scrollController: _controller,
+      selectionOverlay: null,
+      onSelectedItemChanged: (int value) {
+        widget.selectedCallBack.call(widget.timeRangeList[value]);
+        setState(() {
+          currentIndex = value;
+        });
+      },
+      itemExtent: 75,
+      childCount: widget.timeRangeList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          margin: const EdgeInsets.symmetric(vertical: 20),
+          child: Text(
+            '${widget.timeRangeList[index]}${widget.unit}',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize:
+                    widget.timeRangeList.indexOf(widget.timeRangeList[index]) ==
+                            currentIndex
+                        ? 18
+                        : 16),
+          ),
+        );
+      },
+    ));
+  }
+}
