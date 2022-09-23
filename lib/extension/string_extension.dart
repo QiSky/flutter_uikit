f7552c3f286fc7a3a81502f03667cef6577a5b9b
qifanxin <2511242638@qq.com>
2022-09-23 23:16:49 +0800

    修改邮箱的验证

--- lib/extension/string_extension.dart
+++ lib/extension/string_extension.dart
@@ -53,7 +53,7 @@ extension RegularHelper on String {
       r'^((13[0-9])|(14[0-9])|(15[0-9])|(16[0-9])|(17[0-9])|(18[0-9])|(19[0-9]))\d{8}$';
 
   static const String _emailExp =
-      r'^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$';
+      r'^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*\$';
 
   static const String _domainExp =
       r'^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$';
